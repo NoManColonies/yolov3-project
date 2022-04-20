ccfaaea10e4fa4809e03ad686c22a600b3813e6d
NoManColonies <katsunari0908@gmail.com>
2022-04-20 11:23:00 +0700

    Debug the returned value from net.getUnconnectedLayers function

--- camera.py
+++ camera.py
@@ -197,6 +197,7 @@ class CameraInstance:
             layersNames = net.getLayerNames()
             # outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
             try:
+                print([type(i) for i in net.getUnconnectedOutLayers()])
                 outputNames = [(layersNames[(i if type(i) is not type(list()) else len(i)) - 1])
                                for i in net.getUnconnectedOutLayers()]
                 # Feed data to the network
