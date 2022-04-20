516317d6dc8f52d5c26bac273e9d3707754aa24c
NoManColonies <katsunari0908@gmail.com>
2022-04-20 11:16:15 +0700

    Implement runtime type checking because for some reason production machine return list?

--- camera.py
+++ camera.py
@@ -197,8 +197,7 @@ class CameraInstance:
             layersNames = net.getLayerNames()
             # outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
             try:
-                print([(i) for i in net.getUnconnectedOutLayers()])
-                outputNames = [(layersNames[i - 1])
+                outputNames = [(layersNames[(i if type(i) is not list() else len(i)) - 1])
                                for i in net.getUnconnectedOutLayers()]
                 # Feed data to the network
                 outputs = net.forward(outputNames)
